06effcfef34dccaca12fd66bf87609fe356ec882
ashish <ashishsahu496@gmail.com>
2025-06-24 00:52:12 +0530

    all file added

---
+++ SingleTonDesignPatten/src/pattern/Main.java
@@ -0,0 +1,19 @@
+package pattern;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+	Samosa s1=Samosa.getSamosa();
+	Samosa s2 =Samosa.getSamosa();
+	
+	System.out.println(s1.hashCode());
+	System.out.println(s2.hashCode());
+	
+	System.out.println(Jalebi.getJalebi().hashCode());
+	System.out.println(Jalebi.getJalebi().hashCode());
+
+	}
+
+}
